795b6b2de0a98e520c9de08032687b465fd187f2
unknown <rickykitano7@gmail.com>
2021-07-28 12:00:05 -0300

    add Implementation add function

--- js/main.js
+++ js/main.js
@@ -1,45 +1,69 @@
  // essa função faz com oque tiver dentro dela só carregue depois que todo o HTML carregou
-
  window.onload = function() {
+     contas = [];
 
      // para carregar o html primeiro e depois executar, tem que chamar aqui dentro. no caso oque eu fiz ai foi uma excessão, porque to chamando uma função em um clique
      // do botão no HTML
 
- };
+     $('#cadastrar').click(function() {
 
- function PreencherGastos() {
-     //AQUI SÃO INSERIDOS OS VALORES DAS CONTAS
-     var contas = alert("Você poderá inserir os gastos do mês a seguir, se for separar os decimais, utilize ponto ao invés de vírgula:")
-     var despAgua = Number(prompt("Digite a despesa com à conta de agua"))
-     var despEnergia = Number(prompt("Digite a despesa com a conta de energia"))
-     var despCartaoSantander = Number(prompt("Digite a despesa com o cartão do Santander"))
-     var despNubank = Number(prompt("Digite a despesa com o Nubank"))
-     var despAluguel = Number(prompt("Digite a despesa com o aluguel"))
-     var despEmbracon = Number(prompt("Digite a despesa com a Embracon"))
-     var despRastreamento = Number(prompt("Digite a despesa com o Rastreamento"))
-     var despInternet = Number(prompt("Digite a despesa com a internet"))
-     var despCartaoEli = Number(prompt("Digite a despesa com o cartão da Elieuda"))
-     var despCelular = Number(prompt("Digite o valor gasto com o plano de celular"))
+         var contaAtual = lerDados();
 
-     //NESTE É ADICIONADO MAIS ALGUMA DESPESA(TEXTO), QUE FICA CAPTURADO NA MEMÓRIA DA APLICAÇÃO
-     var despOutroTexto = prompt("Digite alguma outra despesa:")
+         validarDados(contaAtual)
+         if (validarDados() != false) {
+             listarContas()
 
-     //ONDE É CAPTURADO O VALOR NESSA VARIÁVEL
-     var despOutroValor = Number(prompt(`Digite o valor da despesa com o(a) ${despOutroTexto}:`))
+             contas.push(contaAtual);
 
-     // ARRAY ONDE É ARMAZENADO TODOS OS VALORES DAS VARIÁVEIS.
-     var despesas = [despAgua, despEnergia, despCartaoSantander, despNubank, despAluguel, despEmbracon, despRastreamento, despInternet, despCartaoEli, despCelular, despOutroValor]
-         //var despesas = [despAgua, despEnergia]
+             var totalContas = somarContas()
 
-     //USA-SE O REDUCE PARA SOMAR TODOS OS ITENS DO ARRAY E O TO.FIXED PARA FIRMAR APENAS 2 CASAS DECIMAIS
-     var contasApagar = despesas.reduce((x, y) => x + y).toFixed(2)
+             exibirTotal(totalContas);
 
-     //AQUI É ONDE É APRESENTADO O VALOR TOTAL DAS CONTAS JÁ MANIPULANDO A DOM
-     var h2Dom = document.querySelector('#total-contas')
-     h2Dom.innerHTML = "O total de contas no mês foi de:" + contasApagar
+             var nome = $("#nome");
+             var valor = $('#valor');
+             limparDados(nome, valor)
+         }
+     })
+ }
 
-     //AQUI É ONDE FIXAMOS DUAS CASAS DECIMAIS E APRESENTAMOS DISCRIMINADAMENTE TODOS OS GASTOS NA DOM
-     var h3Dom = document.querySelector('#contas-mes')
-     h3Dom.innerHTML = `Os valores das despesas desse mês foram os seguintes: <br> Conta de Água: R$  ${despAgua.toFixed(2)}<br>Conta de Energia: R$ ${despEnergia.toFixed(2)}<br>Cartão Santander: R$ ${despCartaoSantander.toFixed(2)}<br>Nubank: R$ ${despNubank.toFixed(2)}<br>Aluguel: R$ ${despAluguel.toFixed(2)}<br>Embracon: R$ ${despEmbracon.toFixed(2)}<br>Rastreamento: R$ ${despRastreamento.toFixed(2)}<br>Internet: R$ ${despInternet.toFixed(2)}<br>Cartão Elieuda: R$ ${despCartaoEli.toFixed(2)}<br>Plano Celular: R$ ${despCelular.toFixed(2)}<br>${despOutroTexto}: R$  ${despOutroValor.toFixed(2)}`
 
+ function limparDados(nome, valor) {
+     nome.val('');
+     valor.val('');
+ }
+
+ function lerDados() {
+     conta = {
+         valor: +$('#valor').val(),
+         nome: $('#nome').val()
+     }
+     return conta
+ }
+
+ function listarContas() {
+     return $('#contas-mes').append(`<h3>${conta.nome}: R$ ${conta.valor.toFixed(2)}</h3>`)
+ }
+
+ function validarDados() {
+     if (conta.nome == '') {
+         alert("Favor informar o nome da conta")
+         return false
+     } else if (conta.valor <= 0) {
+         alert("Favor informar o valor da conta")
+         return false
+     }
+     return true
+ }
+
+ function somarContas() {
+
+     var total = 0
+     contas.forEach(valorConta => {
+         total = total + valorConta.valor
+     })
+     return total;
+ }
+
+ function exibirTotal(total) {
+     return $('#total-contas').text(`O total de contas deste mês foi de: R$ ${total.toFixed(2)}`)
  }
